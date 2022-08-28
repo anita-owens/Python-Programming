36419055e103c0ba04eb65a110f721ab58e531e9
Anita Owens <anita.owens@hyperisland.se>
2022-08-28 08:07:55 +0200

    Create automation for random names function

---
+++ Choose_random_names_automation.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import random
+import os
+from datetime import date
+from datetime import datetime
+
+
+# In[2]:
+
+
+def ChooseTwice(items):
+    a = random.choice(items)
+    b = random.choice(items)
+    return a, b
+
+
+# In[3]:
+
+
+names = ["Alice", "Bob", "Charlie", "Debra", "Peter", "Cora", "Robert", "Violet"]
+
+
+# In[4]:
+
+
+today = date.today()
+dt = datetime.now()
+
+
+# In[5]:
+
+
+(one, two) = ChooseTwice(names)
+if one == two:
+    print("%s is happy!" % one, dt)
+else:
+    print("%s likes %s!" % (one, two), dt)
+
+
+# In[6]:
+
+
+#Write and append results to a text file
+with open('/Users/anitaowens/Documents/GitHub/Python-Programming/file.txt','a') as f:
+    (one, two) = ChooseTwice(names)
+    if one == two:
+        print("%s is happy!" % one, dt, file=f)
+    else:
+        print("%s likes %s!" % (one, two), dt, file=f)
+
+
+# In[7]:
+
+
+currentDirectory = os.getcwd()
+#print(currentDirectory)
